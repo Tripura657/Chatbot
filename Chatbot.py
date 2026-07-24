40c232d34336da00a248c18ed6678d1c3e8921f2
GAJULA TRIPURA TEJASRI <tripurateja4545@gmail.com>
2026-07-24 16:27:14 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,33 +1,33 @@
 import streamlit as st
 from google import genai
-API_KEY = "AIzaSyDOqZVVnc-IPirAsofSxidr46jAsOoYqZk"
 
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
-if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history=[])
-
-st.title("🤖 Chatbot - Your AI Assistant")
-st.write("This is a chatbot made by Sreejitha.......")
+st.title("Chatbot - Your AI Assistant")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
 
-if prompt := st.chat_input("Say something..."):
-    # Add user message to chat history
+prompt = st.chat_input("Say something...")
+
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.markdown(prompt)
-    response = st.session_state.chat.send_message(prompt)
 
-    st.session_state.messages.append({"role": "assistant", "content": response.text})
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    reply = response.text
+
+    st.session_state.messages.append({"role": "assistant", "content": reply})
+
     with st.chat_message("assistant"):
-        st.markdown(response.text)
-        
-
-
+        st.markdown(reply)
